cf7bdc095e2aaa6b1519e50781dac3955715d864
esumitra <esumitra@cainc.com>
2019-02-05 08:58:51 -0500

    update cats and circe dependencies
    resolves https://github.com/esumitra/basic-scala-cats.g8/issues/3

--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -6,14 +6,16 @@ object Dependencies {
     "org.specs2" %% "specs2-scalacheck" % "4.0.0" % "test"
   )
 
-  val circeVersion = "0.9.1"
+  val circeVersion = "0.11.0"
+  val catsVersion = "1.5.0"
   lazy val core = Seq(
-    "org.typelevel" %% "cats-core" % "1.0.1",
-    "org.typelevel" %% "mouse" % "0.16",
-    "com.typesafe" % "config" % "1.3.1",
+    "org.typelevel" %% "cats-core" % catsVersion,
+    "org.typelevel" %% "cats-kernel" % catsVersion,
+    "org.typelevel" %% "cats-macros" % catsVersion,
+    "org.typelevel" %% "mouse" % "0.20",
     "io.circe" %% "circe-core" % circeVersion,
     "io.circe" %% "circe-generic" % circeVersion,
-    "io.circe" %% "circe-parser" % circeVersion,
-    "io.circe" %% "circe-config" % "0.4.1"
+    "com.typesafe" % "config" % "1.3.1",
+    "io.circe" %% "circe-config" % "0.6.1"
   )
 }
