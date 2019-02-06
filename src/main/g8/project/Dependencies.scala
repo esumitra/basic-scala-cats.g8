34c6698fb95c1ce74786eb06fb3a10e5290e1652
esumitra <esumitra@cainc.com>
2019-02-06 06:01:08 -0500

    use pureconfig for configuration
    resolves https://github.com/esumitra/basic-scala-cats.g8/issues/1

--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -9,13 +9,17 @@ object Dependencies {
   val circeVersion = "0.11.0"
   val catsVersion = "1.5.0"
   lazy val core = Seq(
+    // cats FP libary
     "org.typelevel" %% "cats-core" % catsVersion,
     "org.typelevel" %% "cats-kernel" % catsVersion,
     "org.typelevel" %% "cats-macros" % catsVersion,
     "org.typelevel" %% "mouse" % "0.20",
+
+    // support for JSON formats
     "io.circe" %% "circe-core" % circeVersion,
     "io.circe" %% "circe-generic" % circeVersion,
-    "com.typesafe" % "config" % "1.3.1",
-    "io.circe" %% "circe-config" % "0.6.1"
+
+    // support for typesage configuration
+    "com.github.pureconfig" %% "pureconfig" % "0.10.1"
   )
 }
--- src/main/g8/src/main/scala/$package$/MainApp.scala
+++ src/main/g8/src/main/scala/$package$/MainApp.scala
@@ -3,15 +3,19 @@
   */
 
 package $package$
-import $package$.config.ConfigUtils
+
+import $package$.config.{ConfigUtils, CookieSettings}
+import pureconfig.generic.auto._
 
 object MainApp {
+
+  val COOKIE_CONFIG_PATH="$package$.cookie"
 
   def hello(name: String): String = s"Hello \${name}"
 
   def main(args: Array[String]): Unit = {
-    val ttl = ConfigUtils.config.cookie.ttl
-    println(s"running application version with ttl: \${ttl}")
+    val cookie = ConfigUtils.loadAppConfig[CookieSettings](COOKIE_CONFIG_PATH)
+    println(s"running application version with ttl: \${cookie.ttl}")
     val message = args.length match {
       case 0 => hello("Anonymous")
       case _ => hello(args(0))
